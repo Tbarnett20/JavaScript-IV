ec820363a3a207363316808746efe5e49e107ca3
Terrance Danckert <tbarnett2020@gmail.com>
2018-05-24 16:51:46 -0400

    Terrance Danckert CS11 (JavaScript-IV)

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,240 @@
 // CODE here for your Lambda Classes
+/*
+ ** CLASS PERSON **
+*/
+
+class Person {
+  constructor(attr) {
+    this.name = attr.name;
+    this.age = attr.age;
+    this.location = attr.location;
+    this.gender = attr.gender;
+  }
+
+  speak() {
+    return `Hello my name is ${this.name}, I am from ${this.location}`;
+  }
+}
+
+/*********************
+ ** CLASS INSTRUCTOR **
+ *********************/
+class Instructor extends Person {
+  constructor(attr) {
+    super(attr);
+    this.specialty = attr.specialty;
+    this.favLanguage = attr.favLanguage;
+    this.catchPhrase = attr.catchPhrase;
+  }
+
+  demo(subject) {
+    return `Today we are learning about ${subject}`;
+  }
+
+  grade(studentOBJ, subjectSTR) {
+    return `${
+      studentOBJ.name
+    } receives a nobel peace award for his work on ${subjectSTR}`;
+  }
+
+  graduateStudent(studentOBJ) {
+    return `Congratulations, ${
+      studentOBJ.name
+    }, you are now ready to take over the world!`;
+  }
+
+  gradeStudent(studentOBJ) {
+    let newGrade = 0;
+
+    while (newGrade <= 70) {
+      newGrade = Math.floor(Math.random() * Math.floor(100)) + studentOBJ.grade;
+
+      if (newGrade > 70) return this.graduateStudent(studentOBJ);
+    }
+  }
+}
+
+/******************
+ ** CLASS STUDENT **
+ ******************/
+class Student extends Person {
+  constructor(attr) {
+    super(attr);
+    this.previousBackground = attr.previousBackground;
+    this.className = attr.className;
+    this.favSubjects = attr.favSubjects;
+    this.grade = attr.grade;
+  }
+
+  listsSubjects() {
+    for (let i = 0; i < this.favSubjects.length; i++) {
+      console.log(this.favSubjects[i]);
+    }
+  }
+
+  PRAssignment(subjectSTR) {
+    return `${this.name} has submitted a PR for ${subjectSTR}`;
+  }
+
+  sprintChallenge(sprintSTR) {
+    return `${this.name} has begun spring challenge on ${sprintSTR}`;
+  }
+}
+
+/*************************
+ ** CLASS PROJECTMANAGER **
+ *************************/
+class ProjectManager extends Instructor {
+  constructor(attr) {
+    super(attr);
+    this.gradClassName = attr.gradClassName;
+    this.favInstructor = attr.favInstructor;
+  }
+
+  standUp(chanSTR) {
+    return `${this.name} announces to ${chanSTR}, @channel stand-up time!`;
+  }
+
+  debugsCode(studentOBJ, subjectSTR) {
+    return `${this.name} debugs ${studentOBJ.name}'s code on ${subjectSTR}`;
+  }
+}
+
+//  INSTRUCTORS
+
+// Josh
+const Josh = new Instructor({
+  name: "Josh",
+  age: 99,
+  location: "Remote",
+  gender: "Male",
+  specialty: "Front End",
+  favLanguage: "JavaScript",
+  catchPhrase: `Don't forget the homies`
+});
+
+// Jimmy
+const Jimmy = new Instructor({
+  name: "Jimmy",
+  age: 100,
+  location: "Hidden",
+  gender: "Male",
+  specialty: "React",
+  favLanguage: "Javascript",
+  catchPhrase: "Flamingo"
+});
+
+/************************
+ ** STUDENT **
+ ************************/
+// Terrance
+const Terrance = new Student({
+  name: "Terrance",
+  age: 23,
+  location: "KY",
+  gender: "Male",
+  previousBackground: "Student",
+  className: "CS11",
+  favSubjects: ["Nothing", "Frontend"],
+  grade: Math.floor(Math.random() * Math.floor(100))
+});
+
+// Adrian
+const Adrian = new Student({
+  name: "Adrian",
+  age: 600,
+  location: "???",
+  gender: "Male",
+  previousBackground: "previousBackground",
+  className: "CS11",
+  favSubjects: ["favSubjects"],
+  grade: Math.floor(Math.random() * Math.floor(100))
+});
+
+// Austin
+const Austin = new Student({
+  name: "Austin",
+  age: 600,
+  location: "???",
+  gender: "Male",
+  previousBackground: "previousBackground",
+  className: "CS11",
+  favSubjects: ["favSubjects"],
+  grade: Math.floor(Math.random() * Math.floor(100))
+});
+
+/********************************
+ ** INSTANTIATE PROJECT MANAGER **
+ ********************************/
+// Ben
+const Ben = new ProjectManager({
+  name: "Ben",
+  age: 600,
+  location: "???",
+  gender: "Male",
+  previousBackground: "Flamingo Spotting",
+  className: "CS11",
+  favSubjects: ["Flamingos"],
+  grade: Math.floor(Math.random() * Math.floor(100))
+});
+
+// Patrick
+const patrick = new ProjectManager({
+  name: "Patrick",
+  age: 890,
+  location: "???",
+  gender: "Male",
+  specialty: "Something",
+  favLanguage: "JavaScript",
+  catchPhrase: "Life is like a box of chocolates",
+  favInstructor: "favInstructor"
+});
+
+/********************
+ ** INSTRUCTOR TEST **
+ ********************/
+// Josh
+console.log(Josh);
+console.log(Josh.demo("ES6 Classes"));
+console.log(Josh.grade(Terrance, "everything!"));
+
+// Jimmy
+console.log(Jimmy);
+console.log(Jimmy.demo("ES6 Classes"));
+console.log(Jimmy.grade(Terrance, "nothing!"));
+
+/******************
+ ** STUDENTS TEST **
+ ******************/
+// Terrance
+console.log(Terrance);
+Terrance.listsSubjects();
+console.log(Terrance.PRAssignment("JavaScript-IV"));
+console.log(Terrance.sprintChallenge("JavaScript-IV"));
+
+// Adrian
+console.log(Adrian);
+Adrian.listsSubjects();
+console.log(Adrian.PRAssignment("JavaScript-IV"));
+console.log(Adrian.sprintChallenge("JavaScript-IV"));
+
+// Austin
+console.log(Austin);
+Austin.listsSubjects();
+console.log(Austin.PRAssignment("JavaScript-IV"));
+console.log(Austin.sprintChallenge("JavaScript-IV"));
+
+/*************************
+ ** PROJECT MANAGER TEST **
+ *************************/
+// Ben
+console.log(Ben);
+console.log(Ben.standUp("#cs11_flamingos"));
+console.log(Ben.debugsCode(Adrian, "JavaScript"));
+console.log(Ben.gradeStudent(Adrian));
+
+// Patrick
+console.log(patrick);
+console.log(patrick.standUp("#cs10_Patrick"));
+console.log(patrick.debugsCode(Austin, "nothing, he's doing great!"));
+console.log(patrick.gradeStudent(Austin));
--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -2,39 +2,41 @@
 // Today your goal is to refactor all of this code to use ES6 Classes.
 // The console.log() statements should still return what is expected of them.
 
-function GameObject(options) {
-  this.createdAt = options.createdAt;
-  this.dimensions = options.dimensions;
+class GameObject {
+  constructor(options) {
+    this.createdAt = options.createdAt;
+    this.dimensions = options.dimensions;
+  }
+
+  destroy() {
+    return `Object was removed from the game.`;
+  }
 }
 
-GameObject.prototype.destroy = function() {
-  return `Object was removed from the game.`;
-};
+class CharacterStats extends GameObject {
+  constructor(characterStatsOptions) {
+    super(characterStatsOptions);
+    this.hp = characterStatsOptions.hp;
+    this.name = characterStatsOptions.name;
+  }
 
-function CharacterStats(characterStatsOptions) {
-  GameObject.call(this, characterStatsOptions);
-  this.hp = characterStatsOptions.hp;
-  this.name = characterStatsOptions.name;
+  takeDamage() {
+    return `${this.name} took damage.`;
+  }
 }
 
-CharacterStats.prototype = Object.create(GameObject.prototype);
+class Humanoid extends CharacterStats {
+  constructor(humanoidOptions) {
+    super(humanoidOptions);
+    this.faction = humanoidOptions.faction;
+    this.weapons = humanoidOptions.weapons;
+    this.language = humanoidOptions.language;
+  }
 
-CharacterStats.prototype.takeDamage = function() {
-  return `${this.name} took damage.`;
-};
-
-function Humanoid(humanoidOptions) {
-  CharacterStats.call(this, humanoidOptions);
-  this.faction = humanoidOptions.faction;
-  this.weapons = humanoidOptions.weapons;
-  this.language = humanoidOptions.language;
+  greet() {
+    return `${this.name} offers a greeting in ${this.language}.`;
+  }
 }
-
-Humanoid.prototype = Object.create(CharacterStats.prototype);
-
-Humanoid.prototype.greet = function() {
-  return `${this.name} offers a greeting in ${this.language}.`;
-};
 
 const mage = new Humanoid({
   createdAt: new Date(),
@@ -43,11 +45,11 @@ const mage = new Humanoid({
     width: 1,
     height: 1
   },
-  hp: 5,
-  name: 'Bruce',
-  faction: 'Mage Guild',
-  weapons: ['Staff of Shamalama'],
-  language: 'Common Toungue'
+  hp: 5000,
+  name: "Terrance",
+  faction: "Mage Guild",
+  weapons: ["Staff of Shamalama"],
+  language: "Common Toungue"
 });
 
 const swordsman = new Humanoid({
@@ -58,10 +60,10 @@ const swordsman = new Humanoid({
     height: 2
   },
   hp: 15,
-  name: 'Sir Mustachio',
-  faction: 'The Round Table',
-  weapons: ['Giant Sword', 'Shield'],
-  language: 'Common Toungue'
+  name: "Sir Mustachio",
+  faction: "The Round Table",
+  weapons: ["Giant Sword", "Shield"],
+  language: "Common Toungue"
 });
 
 const archer = new Humanoid({
@@ -72,16 +74,16 @@ const archer = new Humanoid({
     height: 4
   },
   hp: 10,
-  name: 'Lilith',
-  faction: 'Forest Kingdom',
-  weapons: ['Bow', 'Dagger'],
-  language: 'Elvish'
+  name: "Lilith",
+  faction: "Forest Kingdom",
+  weapons: ["Bow", "Dagger"],
+  language: "Elvish"
 });
 
 console.log(mage.createdAt); // Today's date
 console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
 console.log(swordsman.hp); // 15
-console.log(mage.name); // Bruce
+console.log(mage.name); // Terrance
 console.log(swordsman.faction); // The Round Table
 console.log(mage.weapons); // Staff of Shamalama
 console.log(archer.language); // Elvish
